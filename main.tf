f577bcd535f9aea7b3b46bf4ede64faf683b09df
JD Dela Cruz <jdarwindelacruz@gmail.com>
2024-01-03 14:48:15 -0800

    retro 2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_providers {  
+  required_providers {
     # https://registry.terraform.io/browse/providers
     aws = {
       source  = "hashicorp/aws"
@@ -14,10 +14,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {  # resource type (aws_instance) and resource name (app_server)
-# AWS ami finder: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html#finding-quick-start-ami
+resource "aws_instance" "app_server" { # resource type (aws_instance) and resource name (app_server)
+  # AWS ami finder: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html#finding-quick-start-ami
   #ami = "ami-0688ba7eeeeefe3cd"         
-  ami = "ami-830c94e3"
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
